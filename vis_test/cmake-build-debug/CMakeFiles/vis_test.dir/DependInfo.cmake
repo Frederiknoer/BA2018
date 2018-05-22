810f3666f4b5832a2122ef06d73d4bf39a38a2c9
Unknown <Frederikk@live.dk>
2018-05-22 14:32:50 +0200

    pat fixes..

---
+++ vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fred/Git/BA2018/vis_test/Algorithms.cpp" "/home/fred/Git/BA2018/vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/Algorithms.cpp.o"
+  "/home/fred/Git/BA2018/vis_test/OpenCV.cpp" "/home/fred/Git/BA2018/vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/OpenCV.cpp.o"
+  "/home/fred/Git/BA2018/vis_test/PclPlane.cpp" "/home/fred/Git/BA2018/vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/PclPlane.cpp.o"
+  "/home/fred/Git/BA2018/vis_test/main.cpp" "/home/fred/Git/BA2018/vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/main.cpp.o"
+  "/home/fred/Git/BA2018/vis_test/rsCam.cpp" "/home/fred/Git/BA2018/vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/rsCam.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkIOExport_AUTOINIT=1(vtkIOExportOpenGL2)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/vtk-8.1"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/local/include/pcl-1.8"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vis_test/cmake-build-debug/CMakeFiles/vis_test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vis_test.dir/main.cpp.o"
+  "CMakeFiles/vis_test.dir/PclPlane.cpp.o"
+  "CMakeFiles/vis_test.dir/rsCam.cpp.o"
+  "CMakeFiles/vis_test.dir/OpenCV.cpp.o"
+  "CMakeFiles/vis_test.dir/Algorithms.cpp.o"
+  "vis_test.pdb"
+  "vis_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vis_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
